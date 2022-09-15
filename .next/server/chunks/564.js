90404daa3a5a4980ff5e6f692452879b6c0a0c53
Adil <adilzhapar2002@gmail.com>
2022-09-16 02:31:29 +0600

    added things to gh-pages

---
+++ .next/server/chunks/564.js
@@ -0,0 +1,98 @@
+exports.id = 564;
+exports.ids = [564];
+exports.modules = {
+
+/***/ 810:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "Description_container__RRnQ_",
+	"subcontainer": "Description_subcontainer__EIIqF",
+	"title": "Description_title__4uV__",
+	"description": "Description_description__h0V__",
+	"firstDescription": "Description_firstDescription__3zYfc",
+	"descriptionWithImage": "Description_descriptionWithImage__c3Uso",
+	"image": "Description_image__J9e9n",
+	"italic": "Description_italic__LYape"
+};
+
+
+/***/ }),
+
+/***/ 564:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Description)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(810);
+/* harmony import */ var _styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+function Description() {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().container),
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().subcontainer),
+            children: [
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                    className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().title),
+                    children: [
+                        "HELLO,",
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                            children: "TURING!"
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                    className: `${(_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().description)} ${(_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().firstDescription)}`,
+                    children: [
+                        "\u041C\u0435\u043D\u0442\u043E\u0440\u0441\u043A\u0430\u044F \u043F\u0440\u043E\u0433\u0440\u0430\u043C\u043C\u0430 \u201CH",
+                        ">",
+                        "T!\u201D \u0431\u044B\u043B\u0430 \u043D\u0430\u0447\u0430\u0442\u0430 \u0432 2018 \u0433\u043E\u0434\u0443 \u0441 \u0436\u0435\u043B\u0430\u043D\u0438\u0435\u043C \u043F\u043E\u043C\u043E\u0447\u044C \u0441\u0442\u0443\u0434\u0435\u043D\u0442\u0430\u043C \u043D\u0430\u0439\u0442\u0438 \u0441\u0432\u043E\u0439 \u043F\u0443\u0442\u044C ",
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                            className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().italic),
+                            children: "\u0434\u0436\u0435\u0434\u0430\u044F"
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                    className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().descriptionWithImage),
+                    children: [
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                            className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().description),
+                            children: [
+                                "\u0432 \u043E\u0431\u0448\u0438\u0440\u043D\u043E\u043C \u043C\u0438\u0440\u0435 Software Engineering \u043F\u043E\u0441\u0440\u0435\u0434\u0441\u0442\u0432\u043E\u043C \u043D\u0435\u043F\u0440\u0435\u0440\u044B\u0432\u043D\u043E\u0433\u043E \u043C\u0435\u043D\u0442\u043E\u0440\u0441\u0442\u0432\u0430 \u0438 \u0432",
+                                " ",
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                                    className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().italic),
+                                    children: "\u043E\u0431\u0445\u043E\u0434 \u0443\u043D\u0438\u0432\u0435\u0440\u0441\u0438\u0442\u0435\u0442\u0441\u043A\u043E\u0439 \u0431\u044E\u0440\u043E\u043A\u0440\u0430\u0442\u0438\u0438"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            src: "/turing.png",
+                            width: 400,
+                            className: (_styles_Description_module_css__WEBPACK_IMPORTED_MODULE_2___default().image),
+                            height: 400
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+};
+
+
+/***/ })
+
+};
+;
---
+++ .next/server/chunks/938.js
@@ -0,0 +1,107 @@
+exports.id = 938;
+exports.ids = [938];
+exports.modules = {
+
+/***/ 316:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"header": "Welcome_header__H_u2H",
+	"buttonsBlock": "Welcome_buttonsBlock__tGLkM",
+	"headerButton": "Welcome_headerButton__LIG9S",
+	"logoBlock": "Welcome_logoBlock__XUyiN",
+	"headerContainer": "Welcome_headerContainer__bnZGt",
+	"title": "Welcome_title__lTr6_",
+	"callToActionContainer": "Welcome_callToActionContainer__H3Iha",
+	"description": "Welcome_description__5XZqt",
+	"actionButton": "Welcome_actionButton__Ahsvk",
+	"descriptionContainer": "Welcome_descriptionContainer___2FlI",
+	"new": "Welcome_new__ezxsJ"
+};
+
+
+/***/ }),
+
+/***/ 938:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Welcome)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(316);
+/* harmony import */ var _styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+function Welcome() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().headerContainer),
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+                className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().header),
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().buttonsBlock)
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().logoBlock),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                src: "/kbtuLogo.svg",
+                                alt: "Vercel Logo",
+                                width: 140,
+                                height: 51
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                src: "/turingLogoWhite.png",
+                                alt: "Vercel Logo",
+                                width: 85,
+                                height: 85
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().callToActionContainer),
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().title),
+                        children: "Full-heart mentorship that works"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().descriptionContainer),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().description),
+                                children: "Hello, Turing! \u2014 \u044D\u0442\u043E \u043C\u0435\u043D\u0442\u043E\u0440\u0441\u043A\u0430\u044F \u043F\u0440\u043E\u0433\u0440\u0430\u043C\u043C\u0430 \u043E\u0442 \u0441\u0442\u0430\u0440\u0448\u0435\u043A\u0443\u0440\u0441\u043D\u0438\u043A\u043E\u0432 \u0434\u043B\u044F \u043C\u043B\u0430\u0434\u0448\u0435\u043A\u0443\u0440\u0441\u043D\u0438\u043A\u043E\u0432"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                target: "_blank",
+                                rel: "noreferrer",
+                                href: "https://docs.google.com/forms/d/e/1FAIpQLSdAK72aTAqXNq5PHiHo1rteIeL3fg8MG1VNkf0jFNqPpSNg2A/viewform",
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+                                    className: (_styles_Welcome_module_css__WEBPACK_IMPORTED_MODULE_2___default().actionButton),
+                                    children: "\u0414\u0410\u0412\u0410\u0419 \u0421 \u041D\u0410\u041C\u0418!"
+                                })
+                            })
+                        ]
+                    })
+                ]
+            })
+        ]
+    });
+};
+
+
+/***/ })
+
+};
+;
