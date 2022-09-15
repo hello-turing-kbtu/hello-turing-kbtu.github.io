01cb09362b209cd9cce3f3cfb1e7d37da76d12fd
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-09-15 15:02:05 +0600

    completed version

---
+++ pages/welcome.js
@@ -0,0 +1,45 @@
+import styles from "../styles/Welcome.module.css";
+import Image from "next/image";
+
+export default function Welcome() {
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.header}>
+        <div className={styles.buttonsBlock}>
+          <button className={styles.headerButton}>О сообществе</button>
+          <button className={styles.headerButton}>Программа</button>
+        </div>
+        <div className={styles.logoBlock}>
+          <Image
+            src="/kbtuLogo.svg"
+            alt="Vercel Logo"
+            width={80}
+            height={51}
+          />
+          <Image
+            src="/turingLogoWhite.png"
+            alt="Vercel Logo"
+            width={85}
+            height={85}
+          />
+        </div>
+      </header>
+      <div className={styles.callToActionContainer}>
+        <p className={styles.title}>Full-heart mentorship that works</p>
+        <div className={styles.descriptionContainer}>
+          <p className={styles.description}>
+            Hello, Turing! — это менторская программа от старшекурсников для
+            младшекурсников
+          </p>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdAK72aTAqXNq5PHiHo1rteIeL3fg8MG1VNkf0jFNqPpSNg2A/viewform"
+          >
+            <button className={styles.actionButton}>ДАВАЙ С НАМИ!</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
