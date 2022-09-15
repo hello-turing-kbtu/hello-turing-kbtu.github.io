d43c0a6169eac9450a7bdf0452fdae76235a76fb
Adil <adilzhapar2002@gmail.com>
2022-09-16 01:22:19 +0600

    A-1-export-added

--- pages/graduates.js
+++ pages/graduates.js
@@ -1,6 +1,61 @@
 import styles from "../styles/Graduates.module.css";
 import Image from "next/image";
-import { gradList } from "./gradList";
+// import { gradList } from "./gradList";
+
+const gradList = [
+  {
+    name: "Rustam-Deniz Emirali",
+    imagePath: "/Grads/rus.jpg",
+    now: "iOS Developer at Arbuz.kz & Pinemelon.com",
+  },
+  {
+    name: "Yerlan Termir",
+    imagePath: "/Grads/yerla.jpg",
+    now: "Full-stack Developer at Toptal",
+  },
+  {
+    name: "Meir Mukushev",
+    imagePath: "/Grads/meir.jpeg",
+    now: "Backend Developer at Arbuz.kz & Pinemelon.com",
+  },
+  {
+    name: "Dinmukhamed Siyezkan",
+    imagePath: "/Grads/dima.jpeg",
+    now: "iOS Developer at Strong Team",
+  },
+  {
+    name: "Kerbez Orazgaliyeva",
+    imagePath: "/Grads/kerbez.jpeg",
+    now: "Software Engineer at Google",
+  },
+  {
+    name: "Miron Sokitbayev",
+    imagePath: "/Grads/miron.jpeg",
+    now: "Android Developer at Jusan Bank",
+  },
+  {
+    name: "Nursat Yerbol",
+    imagePath: "/Grads/nursat.jpeg",
+    now: "Android Developer at Airba",
+  },
+  {
+    name: "Yessey Melis",
+    imagePath: "/Grads/yessey.jpeg",
+    now: "Backend developer at Astana Motors",
+  },
+  {
+    name: "Amantay Orynbayev",
+    imagePath: "/Grads/amantay.jpeg",
+    now: "Backend Developer",
+  },
+  {
+    name: "Nurassyl Omar",
+    imagePath: "/Grads/nura.jpg",
+    now: "Backend Team Lead at TDS Media",
+  },
+];
+
+
 
 export default function Graduates() {
   return (
@@ -8,8 +63,8 @@ export default function Graduates() {
       <div className={styles.subcontainer}>
         <p className={styles.title}>Graduates_2019</p>
         <div className={styles.graduates}>
-          {gradList.map((el) => (
-            <div className={styles.card}>
+          {gradList.map((el, index) => (
+            <div key={index} className={styles.card}>
               <Image src={el.imagePath} width={277} height={339}></Image>
               <p className={styles.cardTitle}>{el.name}</p>
               <p className={styles.cardDescription}>{el.now}</p>
